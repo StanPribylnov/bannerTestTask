56a48be5564bb3e8fdc514a1ece9ce3c4937588c
Pribylnov_s <stanislav13pribylnov@gmail.com>
2016-08-30 14:38:09 +0300

    Add player functionality

--- js/main.js
+++ js/main.js
@@ -1,18 +1,151 @@
 /**
  * Created by Stan on 28.08.2016.
  */
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video', {
+        height: '363',
+        width: '202',
+        videoId: '9xKR8Vcjias',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.pauseVideo();
+}
+function playVideo() {
+    player.playVideo();
+}
+
+
 (function () {
     "use strict";
 
-    var elements = {
-        $range: document.getElementById("range"),
-        $product : document.getElementById("product")
-    },
-    defaultVal = elements.$range.defaultValue;
+    /**
+     * Constructor module of Banner
+     *
+     * @constructor Banner
+     * @return {Object}
+     */
+    (function Banner() {
+        var elements = {
+                $range: document.getElementById("range"),
+                $product : document.getElementById("product"),
+                $video : document.getElementById("video")
+            },
+            opts = {
+                stepSize: -221
+            },
+            defaultVal = elements.$range.defaultValue;
 
+        /**
+         * Method toggle video state
+         *
+         * @method isDefaultState
+         * @returns {boolean}
+         */
+        function isDefaultState() {
+            return defaultVal === elements.$range.value;
+        }
 
-    elements.$range.oninput = function() {
-        console.log(elements.$range.value);
-        elements.$product.style.backgroundPositionY = -221 * elements.$range.value + "px";
-    };
+        /**
+         * Method toggle video state
+         *
+         * @method toggleVideo
+         * @returns {void}
+         */
+        function toggleVideo(state) {
+            if (state) {
+                elements.$video.style.display = "block";
+                playVideo();
+            } else {
+                elements.$video.style.display = "none";
+                stopVideo();
+            }
+        }
+
+        /**
+         * Method change image
+         *
+         * @method changeImage
+         * @returns {void}
+         */
+        function changeImage() {
+            elements.$product.style.backgroundPositionY = opts.stepSize * elements.$range.value + "px";
+        }
+
+        /**
+         * Method sets handlers for events
+         *
+         * @method setEventHandlers
+         * @returns {void}
+         */
+        function setEventHandlers() {
+            elements.$range.oninput = function () {
+                changeImage();
+                toggleVideo(isDefaultState());
+            };
+        }
+
+        /**
+         * Method init of this module
+         *
+         * @method init
+         * @return {void}
+         */
+        (function init() {
+            setEventHandlers();
+        }());
+
+        /**
+         property contains the module
+         * default settings
+         *
+         * @property defaultSettings
+         * @type {Object}
+         */
+        Banner.prototype.defaultSettings = {
+            dependencies: {},
+            identifiers: {
+                module: ".product-card"
+            },
+            classes: {},
+            selectors: {
+                payProductBtn: ".btn-link_to_market"
+            },
+            productsIdLimit: 15
+        };
+
+        return Banner;
+    }());
+
 }());
